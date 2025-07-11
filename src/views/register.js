3f7c519a7e701fbbbac1103440ceff8e1f7324ac
Slavi Ovardov <ovardovslav@gmail.com>
2025-07-12 02:20:41 +0300

    migrate register

--- src/views/register.js
+++ src/views/register.js
@@ -1,42 +1,54 @@
-import { saveUserData, updateNav } from '../utils.js';
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
 import { register } from '../data/user.js';
 import { navigate } from '../nav.js';
+import { saveUserData, updateNav } from '../utils.js';
 
-const section = document.getElementById('register-view');
-section.querySelector('form').addEventListener('submit', onRegister);
+const RegisterTemplate = (onRegister) => html`
+<section id="register-view">
+    <article>
+        <h2>Register</h2>
+        <form @submit=${onRegister}>
+            <label>E-mail: <input type="text" name="email"></label>
+            <label>Password: <input type="password" name="password"></label>
+            <label>Repeat: <input type="password" name="rePass"></label>
+            <input type="submit" value="Register">
+        </form>
+    </article>
+</section>`;
 
-export function showRegisterView() {
-    return section;
-}
+export function showRegisterView(ctx) {
+    ctx.render(RegisterTemplate(onRegister));
+    
+    async function onRegister(event) {
+        event.preventDefault();
 
-async function onRegister(event) {
-    event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const rePass = formData.get('rePass');
 
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get('email');
-    const password = formData.get('password');
-    const rePass = formData.get('rePass');
+        try {
+            if (!email || !rePass || !password) {
+                throw new Error('All fields are required');
+            }
+            if (password.length < 3) {
+                throw new Error('Password must be at least 3 characters long!');
+            }
+            if (password != rePass) {
+                throw new Error('Passwords must match!');
+            }
 
-    try {
-        if (!email) {
-            throw new Error('Email is required');
+            const data = await register(email, password);
+            saveUserData({
+                id: data._id,
+                accessToken: data.accessToken
+            });
+
+            updateNav();
+            navigate('catalog-link');
+        } catch (error) {
+            alert(error.message);
         }
-        if (password.length < 3) {
-            throw new Error('Password must be at least 3 characters long!');
-        }
-        if (password != rePass) {
-            throw new Error('Passwords must match!');
-        }
-
-        const data = await register(email, password);
-        saveUserData({
-            id: data._id,
-            accessToken: data.accessToken
-        });
-
-        updateNav();
-        navigate('catalog-link');
-    } catch (error) {
-        alert(error.message);
     }
 }
